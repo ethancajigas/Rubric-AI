6094a265c662dddc661769ded0b5446302788cb2
ethancajigas <your_email@example.com>
2025-01-12 13:34:37 -0800

    going back to gpt-4o-mini because o1 doesn't work

--- rubricai.py
+++ rubricai.py
@@ -486,7 +486,7 @@ def analyze():
     
 
         response = openai.ChatCompletion.create(
-            model="o1-mini",
+            model="gpt-4o-mini",
             messages=[
                 {"role": "system", "content": "You are a teacher grading assignments fairly based on rubrics. You are also a great writer who can create perfect scoring assignments based off of rubrics"},
                 {"role": "user", "content": prompt},
